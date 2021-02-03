707e7ed975d8e80a08787e5954bc63b0c10ff632
Fabrizio <fazione33@gmail.com>
2021-02-03 23:59:32 +0100

    2nd: Primi tentativi con DOMDocument: arrivati al tbody della tabella interessata

--- dom_scraper.php
+++ dom_scraper.php
@@ -10,5 +10,57 @@ ini_set('xdebug.var_display_max_data', '5000');
 
 $url = "https://free-proxy-list.net";
 // $url = "http://www.freeproxylists.net/"; // sito 2 (USARE CURL)
+// $url = "http://info.cern.ch/hypertext/WWW/TheProject.html";
 
-$content = file_get_contents($url,0);
+
+$ch = curl_init();
+
+curl_setopt($ch,CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$content = curl_exec($ch);
+
+$dom = new DOMDocument();
+@$dom->loadHTML($content); // @ => warning suppressor
+// var_dump($doc->childNodes);
+
+// prova: prelevo il titolo
+$title = $dom->getElementsByTagName('title');
+var_dump($title[0]->textContent);
+
+// prelevo i dati dalla tabella proxy (ricerca elemento per id)
+$table = $dom->getElementById('proxylisttable');
+
+var_dump($table->childNodes[1]); //tbody
+
+foreach ($table->childNodes as $elem) {
+    // var_dump($elem->nodeValue);
+    var_dump($elem->childNodes);
+
+    // foreach ($elem->childNodes as $item) {
+    //     var_dump($item[0]);
+    // }
+}
+
+
+
+// $content = file_get_contents($url,0);
+
+// $dom = new DOMDocument();
+// $dom->loadHTML($content);
+
+// $dom = DOMDocument::loadHTML($content);
+
+// var_dump($dom->childNodes);
+
+// $pageDom = new DomDocument();   
+// $searchPage = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
+// @$pageDom->loadHTML($searchPage);
+
+// var_dump($pageDom->childNodes);
+
+// foreach ($doc->childNodes as $key => $value) {
+//     // var_dump($key);
+//     var_dump($value);
+// }
